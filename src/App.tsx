e5cf56e08009d607467245bab27fd11be295b7de
Carlos Alberto Castelo <carloscastelo@ifce.edu.br>
2023-02-27 14:55:57 +0000

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import DaysTable from "./DaysTable";
+import ReactDOMServer from "react-dom/server";
+import prettier from "prettier/standalone";
+import htmlParser from "prettier/parser-html";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
+  const htmlString = ReactDOMServer.renderToString(<DaysTable />);
+  const formattedHtml = prettier.format(htmlString, {
+    parser: "html",
+    plugins: [htmlParser],
+  });
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Preview:</h2>
+      <DaysTable></DaysTable>
+      <h2>HTML:</h2>
+      <textarea style={{ width: "100%", height: "30em" }}>
+        {formattedHtml}
+      </textarea>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
